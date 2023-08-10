aea5214bccea73efb11a66a11e469706790d8fa9
RyoWakabayashi <gfdmkm573@gmail.com>
2023-08-10 15:56:00 +0900

    feat: Add accordion component

--- lib/pines_sample_web/controllers/page_controller.ex
+++ lib/pines_sample_web/controllers/page_controller.ex
@@ -4,6 +4,21 @@ defmodule PinesSampleWeb.PageController do
   def home(conn, _params) do
     # The home page is often custom made,
     # so skip the default app layout.
-    render(conn, :home, layout: false)
+    conn
+    |> assign(:accordions, [
+      %{
+        title: "Accordion 1",
+        message: "Message 1"
+      },
+      %{
+        title: "Accordion 2",
+        message: "Message 2"
+      },
+      %{
+        title: "Accordion 3",
+        message: "Message 3"
+      }
+    ])
+    |> render(:home, layout: false)
   end
 end
